8fdb0563881cfaef66df8e1d5059a08df02d8098
Jonathan Sharifi <jonathansharifi@gmail.com>
2014-09-24 19:35:12 -0700

    Jon: Nicer message on the index page. Still a WIP.

--- edmund-web/src/main/java/net/wazim/edmund/controllers/EdmundIndexServlet.java
+++ edmund-web/src/main/java/net/wazim/edmund/controllers/EdmundIndexServlet.java
@@ -12,7 +12,7 @@ public class EdmundIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         resp.setStatus(HttpServletResponse.SC_OK);
-        resp.getWriter().println("<h1>Edmund thus enabled...</h1>");
+        resp.getWriter().println("<html><head><title>Project Edmund</title><h1>Edmund thus enabled...</h1>Deployed by Travis CI");
     }
 
 }
